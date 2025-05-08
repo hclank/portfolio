76f5f353820d242fdc303b38c6f3e02269db8971
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-05-09 01:10:30 +0530

    shaders are working... gotta make it look cool now

--- app/graphics.ts
+++ app/graphics.ts
@@ -1,6 +1,64 @@
 import { RefObject } from "react";
 import * as three from "three";
 
+const vertex = `
+uniform float time;
+varying vec2 vUv;
+varying vec3 vPosition;
+uniform vec2 pixels;
+float pi = 3.14;
+
+void main() {
+    vUv = uv;
+    vPosition = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+
+const fragment = `
+uniform float time;
+uniform float progress;
+uniform sampler2D texture1;
+uniform vec4 resolution;
+uniform vec2 vUv;
+varying vec3 vPosition;
+float pi = 3.14;
+
+// noise function
+float mod289(float x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 mod289(vec4 x){return x - floor(x * (1.0 / 289.0)) * 289.0;}
+vec4 perm(vec4 x){return mod289(((x * 34.0) + 1.0) * x);}
+
+float noise(vec3 p){
+    vec3 a = floor(p);
+    vec3 d = p - a;
+    d = d * d * (3.0 - 2.0 * d);
+
+    vec4 b = a.xxyy + vec4(0.0, 1.0, 0.0, 1.0);
+    vec4 k1 = perm(b.xyxy);
+    vec4 k2 = perm(k1.xyxy + b.zzww);
+
+    vec4 c = k2 + a.zzzz;
+    vec4 k3 = perm(c);
+    vec4 k4 = perm(c + 1.0);
+
+    vec4 o1 = fract(k3 * (1.0 / 41.0));
+    vec4 o2 = fract(k4 * (1.0 / 41.0));
+
+    vec4 o3 = o2 * d.z + o1 * (1.0 - d.z);
+    vec2 o4 = o3.yw * d.x + o3.xz * (1.0 - d.x);
+
+    return o4.y * d.y + o4.x * (1.0 - d.y);
+}
+
+void main() {
+    float n = noise(vPosition+time);
+    gl_FragColor = vec4(n,0., 0.0, 1.);
+}
+`;
+
+const is_playing = false;
+
 function init(containerRef: RefObject<HTMLDivElement>) {
   const scene = new three.Scene();
   const camera = new three.PerspectiveCamera(
@@ -17,15 +75,23 @@ function init(containerRef: RefObject<HTMLDivElement>) {
   return { scene, camera, renderer };
 }
 
-function graphics(
-  scene: three.Scene,
-  renderer: three.WebGLRenderer,
-  camera: three.PerspectiveCamera
-) {
+export default function Graphics(containerRef: RefObject<HTMLDivElement>) {
+  const { scene, camera, renderer } = init(containerRef);
+
   const geometry = new three.SphereGeometry(1.5, 32, 32);
-  const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+  const material = new three.ShaderMaterial({
+    side: three.DoubleSide,
+    uniforms: {
+      time: { value: 0 },
+      resolution: { value: new three.Vector4() },
+    },
+    vertexShader: vertex,
+    fragmentShader: fragment,
+  });
   const sphere_1 = new three.Mesh(geometry, material);
   scene.add(sphere_1);
+
+  camera.position.set(0, 0, 1.3);
 
   const renderScene = () => {
     renderer.render(scene, camera);
@@ -33,13 +99,6 @@ function graphics(
   };
 
   renderScene();
-}
-
-export default function webgl_stuff(containerRef: RefObject<HTMLDivElement>) {
-  const { scene, camera, renderer } = init(containerRef);
-  camera.position.set(0, 0, 1.3);
-
-  graphics(scene, renderer, camera);
 
   const handleResize = () => {
     const width = window.innerWidth;
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,16 @@
 "use client";
 import { useRef, useEffect } from "react";
-import webgl_stuff from "./graphics";
+import graphics from "./graphics";
 
 const Home = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      webgl_stuff(containerRef);
+      graphics(containerRef);
     }
   }, []);
-  return (
-    <div ref={containerRef}>
-      <h1 className="absolute w-full top-10 text-center z-100 block">
-        hclanka
-      </h1>
-    </div>
-  );
+  return <div ref={containerRef} className="h-screen"></div>;
 };
 
 export default Home;
