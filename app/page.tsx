d4d9651113b70ec2a1ecf1e1378a39055702e84e
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-05-08 14:44:40 +0530

    added threejs and got a cube rendered

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
 "use client";
+import { useRef, useEffect } from "react";
+import * as three from "three";
 
-import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+const Home = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
 
-export default function Home() {
-  return (
-    <div>
-      <Navbar />
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const scene = new three.Scene();
 
-      <Footer />
-    </div>
-  );
-}
+      const camera = new three.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+
+      const renderer = new three.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      containerRef.current?.appendChild(renderer.domElement);
+
+      const geometry = new three.BoxGeometry(1, 1, 1);
+      const material = new three.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new three.Mesh(geometry, material);
+      scene.add(cube);
+
+      camera.position.z = 5;
+
+      renderer.render(scene, camera);
+    }
+  }, []);
+  return <div ref={containerRef} />;
+};
+
+export default Home;
