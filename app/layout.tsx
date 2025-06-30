248afa6a0294a502abfc3166cc8a53ee159ac3fa
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-06-30 14:06:25 +0530

    error in deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
-export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
 export const metadata: Metadata = {
   title: "hclanka",
-  description: "working on startups and freelancing on the side.",
+  description: "ml and hardware enthusiast.",
 };
 
 export default function RootLayout({
@@ -22,12 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
-        )}
-      >
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
