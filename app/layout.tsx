16bb10353894c3c309e6cc27930b8cb602b66fb7
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-08-10 15:55:53 +0530

    changed meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "hclanka",
-  description: "ml and hardware enthusiast.",
+  description: "entreprenuer and ai researcher.",
 };
 
 export default function RootLayout({
